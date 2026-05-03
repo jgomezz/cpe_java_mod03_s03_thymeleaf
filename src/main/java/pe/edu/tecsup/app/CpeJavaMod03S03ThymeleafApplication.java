864c53f24a7b18ec4ad36ed70c4a4b90ee119a33
jgomezz <jgomezz@gmail.com>
2026-05-03 12:50:36 -0500

    first version

---
+++ src/main/java/pe/edu/tecsup/app/CpeJavaMod03S03ThymeleafApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpeJavaMod03S03ThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpeJavaMod03S03ThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/tecsup/app/CpeJavaMod03S03ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpeJavaMod03S03ThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
